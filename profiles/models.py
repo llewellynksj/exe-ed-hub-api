2307a23dbe6d3720dd43155bca494a513a3ace26
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-11-18 10:45:10 +0000

    Add profiles app and parent profile model

---
+++ profiles/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+
+class ParentProfile(models.Model):
+  """
+  Parent Profile Model
+  """
+  created_on = models.DateTimeField(auto_now_add=True)
+  updated_on = models.DateTimeField(auto_now=True)
+  username = models.OneToOneField(User, on_delete=models.CASCADE)
+  image = models.ImageField(
+    upload_to='images/',
+    default='../ExeEdHub/sleep_glasses_vmomqy'
+  )
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+
+  class Meta:
+    ordering = ['-created_on']
+
+  def __str__(self):
+    return f"{self.username}'s profile"
+  
+
+# Function using signals to create profile automatically
+# from CI DRF-API walkthrough:
+def create_profile(sender, instance, created, **kwargs):
+    """
+    Function to create a profile automatically when a User is created
+    """
+    if created:
+        ParentProfile.objects.create(username=instance)
+
+
+post_save.connect(create_profile, sender=User)
