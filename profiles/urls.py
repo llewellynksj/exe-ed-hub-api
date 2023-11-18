431cd5f9726ab6e601900f1f62480c3af1c41f2c
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-11-18 13:06:00 +0000

    Create schools app and add schools model

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from profiles import views
 
+# Router code from 'Routers' chapter of 'The ultimate Django Part 2' course
+# by Code with Mosh: https://codewithmosh.com/p/the-ultimate-django-part2
 router = DefaultRouter()
 router.register('profiles', views.ParentProfileViewSet)
 router.urls
---
+++ schools/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class School(model.Models):
+  """
+  School Model
+  """
+  SCHOOL_LEVEL = [
+    ('Primary', 'Primary'),
+    ('Secondary', 'Secondary'),
+    ('Sixth Form/College', 'Sixth Form/College'),
+  ]
+
+  OFSTED_RATING = [
+    ('Outstanding', 'Outstanding'),
+    ('Good', 'Good'),
+    ('Requires Improvement', 'Requires Improvement'),
+    ('Inadequate', 'Inadequate'),
+  ]
+
+  created_on = models.DateTimeField(auto_now_add=True)
+  updated_on = models.DateTimeField(auto_now=True)
+  school_name = models.CharField(max_length=255)
+  school_level = models.CharField(choices=SCHOOL_LEVEL, max_length=255)
+  street_address = models.CharField(max_length=255)
+  city = models.CharField(max_length=100)
+  postcode = models.CharField(max_length=10)
+  ofsted = models.CharField(choices=OFSTED_RATING, max_length=50)
+
+  class Meta:
+    ordering = ['-school_name']
+  
+  def __str__(self):
+    return f"{self.school_name} - {self.school_level}"
