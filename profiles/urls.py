7c038ec38964e343591e17d646f7152895b7c020
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-11-18 12:19:50 +0000

    Add views for parent profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,5 @@ from profiles import views
 
 urlpatterns = [
     path('profiles/', views.ParentProfileList.as_view()),
-    # path('profiles/<int:pk>/', views.ParentProfileDetail.as_view()),
+    path('profiles/<int:pk>/', views.ParentProfileDetail.as_view()),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,20 @@
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import ParentProfile
 from .serializers import ParentProfileSerializer
 
 
-class ParentProfileList(APIView):
-  def get (self, request):
-    profiles = ParentProfile.objects.all()
-    serializer = ParentProfileSerializer(profiles, many=True)
-    return Response(serializer.data)
+class ParentProfileList(ListCreateAPIView):
+  queryset = ParentProfile.objects.all()
+  serializer_class = ParentProfileSerializer
+  
+  def get_serializer_context(self):
+    return {'request': self.request}
+
+
+class ParentProfileDetail(RetrieveUpdateDestroyAPIView):
+  queryset = ParentProfile.objects.all()
+  serializer_class = ParentProfileSerializer
