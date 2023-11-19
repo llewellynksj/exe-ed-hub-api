ac25351bd41a770a0971e023859886c1f0c508d5
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-11-19 14:20:24 +0000

    Add pagination to settings

--- schools/views.py
+++ schools/views.py
@@ -1,5 +1,5 @@
-from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import School, Review
 from .serializers import SchoolSerializer, ReviewSerializer
