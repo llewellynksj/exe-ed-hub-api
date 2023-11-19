7f92cf7d6b5b777604f829bec07fd20e2819b5e2
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-11-19 09:51:29 +0000

    Add basic view and serializer for review

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
+from .models import Review
+from .serializers import ReviewSerializer
 
-# Create your views here.
+class ReviewViewSet(ModelViewSet):
+  """
+  one view set that combines all operations for viewing list and detail
+  """
+  queryset = Review.objects.all()
+  serializer_class = ReviewSerializer
