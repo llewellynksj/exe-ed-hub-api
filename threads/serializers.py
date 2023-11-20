abe92f218a82c20d775360b87e535ef1b9b5d127
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-11-20 13:43:58 +0000

    Update serializers for post, comment and like

--- threads/serializers.py
+++ threads/serializers.py
@@ -1,12 +1,33 @@
 from django.db import IntegrityError
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Post, Comment, Like
+
+# Adapted from CI DRF-API walkthrough:
 
 class PostSerializer(serializers.ModelSerializer):
   """
   
   """
   username = serializers.ReadOnlyField(source='username.username')
+  is_user = serializers.SerializerMethodField(method_name='get_is_user')
+  profile_id = serializers.ReadOnlyField(source='username.profile.id')
+  profile_image = serializers.ReadOnlyField(source='username.profile.image.url')
+  like_id = serializers.SerializerMethodField(method_name='get_like_id')
+  likes_count = serializers.ReadOnlyField()
+  comments_count = serializers.ReadOnlyField()
+
+  def get_is_user(self, obj):
+    request = self.context['request']
+    return request.user == obj.username
+  
+  def get_like_id(self, obj):
+    user = self.context['request'].user
+    if user.is_authenticated:
+        like = Like.objects.filter(
+            username=user, post=obj).first()
+        return like.id if like else None
+    return None
 
   class Meta:
     model = Post
@@ -15,9 +36,15 @@ class PostSerializer(serializers.ModelSerializer):
       'created_on',
       'updated_on',
       'username',
+      'is_user',
+      'profile_id',
+      'profile_image',
       'title',
       'content',
       'category',
+      'comments_count',
+      'like_id',
+      'like_count',
     ]
 
 
@@ -26,6 +53,31 @@ class CommentSerializer(serializers.ModelSerializer):
   
   """
   username = serializers.ReadOnlyField(source='username.username')
+  is_user = serializers.SerializerMethodField(method_name='get_is_user')
+  profile_id = serializers.ReadOnlyField(source='username.profile.id')
+  profile_image = serializers.ReadOnlyField(source='username.profile.image.url')
+  like_id = serializers.SerializerMethodField(method_name='get_like_id')
+  likes_count = serializers.ReadOnlyField()
+  created_on = serializers.SerializerMethodField(method_name='get_created_on')
+  updated_on = serializers.SerializerMethodField(method_name='get_updated_on')
+
+  def get_is_user(self, obj):
+    request = self.context['request']
+    return request.user == obj.username
+
+  def get_created_on(self, obj):
+    return naturaltime(obj.created_at)
+
+  def get_updated_on(self, obj):
+    return naturaltime(obj.updated_at)
+  
+  def get_like_id(self, obj):
+    user = self.context['request'].user
+    if user.is_authenticated:
+        like = Like.objects.filter(
+            username=user, comment=obj).first()
+        return like.id if like else None
+    return None
 
   class Meta:
     model = Comment
@@ -34,8 +86,13 @@ class CommentSerializer(serializers.ModelSerializer):
       'created_on',
       'updated_on',
       'username',
+      'is_user',
+      'profile_id',
+      'profile_image',
       'post',
       'comment',
+      'like_id',
+      'like_count',
     ]
 
 
@@ -43,7 +100,7 @@ class LikeSerializer(serializers.ModelSerializer):
   """
   
   """
-  owner = serializers.ReadOnlyField(source='owner.username')
+  username = serializers.ReadOnlyField(source='username.username')
 
   class Meta:
       model = Like
