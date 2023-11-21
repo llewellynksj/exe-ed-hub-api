d28a0e700c0847793692874645e4b11e5151e9d4
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-11-21 11:07:22 +0000

    Add views to exchanges app

--- exchanges/views.py
+++ exchanges/views.py
@@ -1,3 +1,34 @@
+from rest_framework import permissions
 from rest_framework.viewsets import ModelViewSet
 from .models import Item, Response
 from .serializers import ItemSerializer, ResponseSerializer
+
+
+class ItemViewSet(ModelViewSet):
+  """
+  
+  """
+  queryset = Item.objects.all()
+  serializer_class = ItemSerializer
+
+  def get_permissions(self):
+    if self.action == 'list':
+        permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    else:
+        permission_classes = [IsOwnerOrReadOnly]
+    return [permission() for permission in permission_classes]
+
+
+class ResponseViewSet(ModelViewSet):
+  """
+  
+  """
+  queryset = Response.objects.all()
+  serializer_class = ResponseSerializer
+
+  def get_permissions(self):
+    if self.action == 'list':
+        permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    else:
+        permission_classes = [IsOwnerOrReadOnly]
+    return [permission() for permission in permission_classes]
